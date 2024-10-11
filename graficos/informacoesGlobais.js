80a82bc44e8e9586905f5915d44fb925ca4926d5
Danilodoamaral <108030205+Danilodoamaral@users.noreply.github.com>
2024-10-11 17:50:15 -0300

    11/10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,6 @@
 const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizadorInformacoesGlobais)()
+async function vizualizadorInformacoesGlobais(){
 const res=await fetch(url)
 const dados=await res.json()
 console.log(dados);
